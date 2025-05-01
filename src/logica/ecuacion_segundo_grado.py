a7dda2d18643cea32ae4ea375df43624e97198e4
JOre <labe402@continental.edu.pe>
2025-04-30 21:16:38 -0500

    Setters y Getters agregados

--- src/logica/ecuacion_segundo_grado.py
+++ src/logica/ecuacion_segundo_grado.py
@@ -7,10 +7,24 @@ import math
 
 class EcuacionSegundoGrado:
     def __init__(self,a=0,b=0,c=0):
-        self.a = a
+        self.__a = a
         self.b = b
         self.c = c
 
+    def set_a(self, a):
+        self.__a = a
+
+    def set_b(self, b):
+        self.b = b
+
+    def set_c(self, c):
+        self.c = c
+
+    def set_all_data(self,a,b,c):
+        self.set_c(c)
+        self.set_b(b)
+        self.set_a(a)
+    
     def validar_dato(self, dato, lista=[]):
         if not type(dato) in [float, int]:
             lista.append(dato)
@@ -19,7 +33,7 @@ class EcuacionSegundoGrado:
     def validar_datos_ecuacion(self):
 
         datos_no_validos = []
-        is_data_valid = (self.validar_dato(self.a, datos_no_validos)
+        is_data_valid = (self.validar_dato(self.__a, datos_no_validos)
                          and self.validar_dato(self.b, datos_no_validos)
                          and self.validar_dato(self.c, datos_no_validos))
         if len(datos_no_validos)>1:
@@ -44,15 +58,15 @@ class EcuacionSegundoGrado:
         if not is_valid:
             return response
 
-        if self.a == 0:
+        if self.__a == 0:
             return "Error! Datos no correspondientes a segundo grado."
 
-        discriminante = (self.b**2)-4*self.a*self.c
+        discriminante = (self.b**2)-4*self.__a*self.c
         is_dis_negative = discriminante < 0
         calc_discriminante = abs(discriminante)
         calc_discriminante = math.sqrt(calc_discriminante)
-        calc_discriminante /= 2*self.a
-        calc_b = (-self.b)/(2*self.a)
+        calc_discriminante /= 2*self.__a
+        calc_b = (-self.b)/(2*self.__a)
 
         calc_b = round(calc_b, 3)
         calc_discriminante = round(calc_discriminante, 3)
